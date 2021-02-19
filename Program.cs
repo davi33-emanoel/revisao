f0f1e08d703e4ca622515e6a75838a92c941c7bd
davi <79283588+davi33-emanoel@users.noreply.github.com>
2021-02-19 17:26:15 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Revisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe a opção desejada");
+            Console.WriteLine("1-Inserir novo aluno ");
+            Console.WriteLine("2 listar alunos ");
+            Console.WriteLine("3- Calcular média geral");
+            Console.WriteLine("X- Sair");
+            Console.WriteLine();
+            
+            string opçaoUsuario = Console.ReadLine();
+
+            while (opçaoUsuario.ToUpper() != "X")
+            {
+                 switch(opçaoUsuario)
+                 {
+                     case "1":
+                        //TODO: adicionar aluno
+                     break;
+
+                     case "2":
+                           //TODO: listar alunos
+                     break;
+
+                     case "3":
+                          //Calcular media geral
+                     break;
+
+                     default:
+                        throw new ArgumentOutOfRangeException();
+           
+                 }
+            Console.WriteLine("Informe a opção desejada");
+            Console.WriteLine("1-Inserir novo aluno ");
+            Console.WriteLine("2 listar alunos ");
+            Console.WriteLine("3- Calcular média geral");
+            Console.WriteLine("X- Sair");
+            Console.WriteLine();
+            
+            opçaoUsuario = Console.ReadLine();
+            }
+
+        }
+    }
+}
